f3fe59cc80a9758eb93f0e83721d2dbb7140670f
Hangil Lim <lhg0993@naver.com>
2020-01-10 22:18:05 +0900

    add retrofit & interface

--- app/src/main/java/com/example/matchmaking/MainActivity.java
+++ app/src/main/java/com/example/matchmaking/MainActivity.java
@@ -4,14 +4,25 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 public class MainActivity extends AppCompatActivity {
 
     EditText loginId;
     EditText loginPw;
+
+    Retrofit retrofit;
+    RetrofitInterface retrofitInterface;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,14 +38,43 @@ public class MainActivity extends AppCompatActivity {
         loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                if(loginId.getText() == null){
+                    Toast.makeText(getApplicationContext(),"아이디 입력",Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                else (loginPw.getText() == null){
+                    Toast.makeText(getApplicationContext(),"비밀번호 입력",Toast.LENGTH_SHORT).show();
+                    return;
+                }
                 //server에 id/pw 보내고 확인받기
+                retrofit = new Retrofit.Builder().baseUrl(retrofitInterface.API_URL).addConverterFactory(GsonConverterFactory.create()).build();
+                retrofitInterface = retrofit.create(RetrofitInterface.class);
 
-                Intent intent = new Intent(getApplicationContext(), MatchMainActivity.class);
-                intent.putExtra("userId",loginId.getText().toString());
-                startActivity(intent);
+                Call<String> comment = retrofitInterface.sendLogin(loginId.getText().toString(), loginPw.getText().toString());
+                comment.enqueue(new Callback<String>() {
+                    @Override
+                    public void onResponse(Call<String> call, Response<String> response) {
+                        String response_ = response.body();
+                        if(response_.equals("성공")){
+                            Intent intent = new Intent(getApplicationContext(), MatchMainActivity.class);
+                            intent.putExtra("userId",loginId.getText().toString());
+                            startActivity(intent);
+                        }
+                        else{
+                            Toast.makeText(getApplicationContext(),"로그인 실패",Toast.LENGTH_SHORT).show();
+                        }
+                    }
+
+                    @Override
+                    public void onFailure(Call<String> call, Throwable t) {
+                        Log.d("MainActivity", t.toString());
+                    }
+                });
+
+
             }
         });
-        
+
         signButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
--- app/src/main/java/com/example/matchmaking/SignActivity.java
+++ app/src/main/java/com/example/matchmaking/SignActivity.java
@@ -2,6 +2,7 @@ package com.example.matchmaking;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -10,7 +11,16 @@ import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
 public class SignActivity extends Activity {
+
+    Retrofit retrofit;
+    RetrofitInterface retrofitInterface;
 
     private User newUser = null;
 
@@ -53,7 +63,28 @@ public class SignActivity extends Activity {
                 newUser = new User(signId.getText().toString(), signPw.getText().toString(), signNic.getText().toString(), signTier.getSelectedItem().toString(), signPosi.getSelectedItem().toString(), signVoic.getSelectedItem().toString());
 
                 //server에 회원가입 요청
+                retrofit = new Retrofit.Builder().baseUrl(retrofitInterface.API_URL).addConverterFactory(GsonConverterFactory.create()).build();
+                retrofitInterface = retrofit.create(RetrofitInterface.class);
 
+                Call<String> comment = retrofitInterface.sendSign(newUser);
+                comment.enqueue(new Callback<String>() {
+                    @Override
+                    public void onResponse(Call<String> call, Response<String> response) {
+                        String response_ = response.body();
+                        if(response_.equals("성공")){
+                            Toast.makeText(getApplicationContext(),"회원가입 성공",Toast.LENGTH_SHORT).show();
+                            finish();
+                        }
+                        else{
+                            Toast.makeText(getApplicationContext(),"회원가입 실패",Toast.LENGTH_SHORT).show();
+                        }
+                    }
+
+                    @Override
+                    public void onFailure(Call<String> call, Throwable t) {
+                        Log.d("SignActivity",t.toString());
+                    }
+                });
             }
         });
     }
