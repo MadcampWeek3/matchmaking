25baffa022a71c1b6b27cfb8f3b4fb980d24a5b2
wtrr59 <wtrr59@naver.com>
2020-01-13 13:07:11 +0900

    chat update

--- app/src/main/java/com/example/matchmaking/MatchRoomActivity.java
+++ app/src/main/java/com/example/matchmaking/MatchRoomActivity.java
@@ -17,6 +17,7 @@ public class MatchRoomActivity extends AppCompatActivity {
     private String userid;
     private MatchRoomRecyclerAdapter matchRoomRecyclerAdapter;
     private RecyclerView recyclerView;
+    private MatchChatRecyclerAdapter matchChatRecyclerAdapter;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -33,11 +34,13 @@ public class MatchRoomActivity extends AppCompatActivity {
         User user2 = new User("Two","Two","Master","정글","가능");
         User user3 = new User("Three","Three","Master","서폿","가능");
         User user4 = new User("Four","Four","Master","원딜","가능");
+        User user5 = new User("Five","Five","Master","탑","가능");
 
         matchRoomRecyclerAdapter.additem(user1);
         matchRoomRecyclerAdapter.additem(user2);
         matchRoomRecyclerAdapter.additem(user3);
         matchRoomRecyclerAdapter.additem(user4);
+        matchRoomRecyclerAdapter.additem(user5);
 
 
         userid = getIntent().getStringExtra("userid");
@@ -55,5 +58,13 @@ public class MatchRoomActivity extends AppCompatActivity {
                 Log.e("MatchRoomActivity",t.getMessage());
             }
         });
+
+        RecyclerView chatrecyclerView = findViewById(R.id.match_room_chat_recycler);
+        matchChatRecyclerAdapter = new MatchChatRecyclerAdapter(userid,"0");
+
+        chatrecyclerView.setLayoutManager(new LinearLayoutManager(this));
+        chatrecyclerView.setAdapter(matchChatRecyclerAdapter);
+
+        matchChatRecyclerAdapter.additem("ㅎㅇㅎㅇ","16:20");
     }
 }
